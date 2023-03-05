40b474637e304fb8fe12e23761a6174506b854eb
Spenser McLaughlin <mcspenser7@gmail.com>
2023-03-04 21:38:14 -0500

    Update card-list.js

--- src/card-list.js
+++ src/card-list.js
@@ -16,21 +16,42 @@ export class CardList extends LitElement {
       <professor-card>
       </professor-card>
       <professor-card
-        author = 'Spenser McLaughlin';
+        author = 'The Italian Stallion';
         professorName = 'Professor David Hozza';
         funFact = 'This is a picture of the best Cyber professor at PSU';
         topText = 'Splunk';
         bottomText = 'Is Not Fun';
         imgURL = 'https://ist.psu.edu/sites/default/files/directory/Hozza-David.jpg';
-        description = 'Details';
-        <slot name="bing"></slot>
+        description = 'More info';
       ></professor-card>
-      <professor-card>
-      </professor-card>
-      <professor-card>
-      </professor-card>
-      <professor-card>
-      </professor-card>
+      <professor-card
+        author = 'Your boy';
+        professorName = 'Professor Vanessa McLaughlin';
+        funFact = 'This is a picture of a CAS 100 professor at PSU';
+        topText = 'Public Speaking';
+        bottomText = 'Or Death';
+        imgURL = 'https://npr.brightspotcdn.com/dims4/default/c879421/2147483647/strip/true/crop/4032x3024+0+0/resize/880x660!/quality/90/?url=http%3A%2F%2Fnpr-brightspot.s3.amazonaws.com%2Flegacy%2Fsites%2Fwpsu%2Ffiles%2F201912%2FIMG-1789.jpg';
+        description = 'The deets';
+      ></professor-card>
+      <professor-card
+        author = 'The Man Himself';
+        professorName = 'Professor Cory Raupers';
+        funFact = 'This is a picture of a Business professor at State College Area High School';
+        topText = 'Let us get down to';
+        bottomText = 'BUSINESS';
+        imgURL = 'https://www.scasd.org/cms/lib/PA01000006/Centricity/Domain/1139/RaupersCory.jpg';
+        description = 'Info';
+      ></professor-card>
+      <professor-card
+        author = 'Spoonsor';
+        professorName = 'Professor Daniel Welch';
+        funFact = 'This is a picture of a programming prof at PSU';
+        topText = 'There is no meme';
+        bottomText = 'Nobody likes Java';
+        imgURL = 'https://ist.psu.edu/sites/default/files/directory/Welch-Dan.jpg';
+        description = 'Some details';
+      ></professor-card>
+      <slot name="bing"></slot>
     `;
   }
 }
