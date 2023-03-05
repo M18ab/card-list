20797d23a516e4d28eb0cbaaadf028ed130a09f2
Spenser McLaughlin <mcspenser7@gmail.com>
2023-03-05 18:47:11 -0500

    Update card-list.js

--- src/card-list.js
+++ src/card-list.js
@@ -14,6 +14,7 @@ export class CardList extends LitElement {
   render() {
     return html`
       <professor-card>
+        <p slot="bing">This is a picture of a bing user</p>
       </professor-card>
       <professor-card
         author = 'The Italian Stallion';
@@ -22,8 +23,9 @@ export class CardList extends LitElement {
         topText = 'Splunk';
         bottomText = 'Is Not Fun';
         imgURL = 'https://ist.psu.edu/sites/default/files/directory/Hozza-David.jpg';
-        description = 'More info';
-      ></professor-card>
+        description = 'More info';>
+        <p slot="bing">Professor Hozza teaches SRA 221 and CYBER 262</p>
+      </professor-card>
       <professor-card
         author = 'Your boy';
         professorName = 'Professor Vanessa McLaughlin';
@@ -31,8 +33,9 @@ export class CardList extends LitElement {
         topText = 'Public Speaking';
         bottomText = 'Or Death';
         imgURL = 'https://npr.brightspotcdn.com/dims4/default/c879421/2147483647/strip/true/crop/4032x3024+0+0/resize/880x660!/quality/90/?url=http%3A%2F%2Fnpr-brightspot.s3.amazonaws.com%2Flegacy%2Fsites%2Fwpsu%2Ffiles%2F201912%2FIMG-1789.jpg';
-        description = 'The deets';
-      ></professor-card>
+        description = 'The deets';>
+        <p slot="bing">She's also an influencer @vanessadotfitness</p>
+      </professor-card>
       <professor-card
         author = 'The Man Himself';
         professorName = 'Professor Cory Raupers';
@@ -40,8 +43,9 @@ export class CardList extends LitElement {
         topText = 'Let us get down to';
         bottomText = 'BUSINESS';
         imgURL = 'https://www.scasd.org/cms/lib/PA01000006/Centricity/Domain/1139/RaupersCory.jpg';
-        description = 'Info';
-      ></professor-card>
+        description = 'Info';>
+        <p slot="bing">He's also a coach of the high school football team</p>
+      </professor-card>
       <professor-card
         author = 'Spoonsor';
         professorName = 'Professor Daniel Welch';
@@ -49,9 +53,9 @@ export class CardList extends LitElement {
         topText = 'There is no meme';
         bottomText = 'Nobody likes Java';
         imgURL = 'https://ist.psu.edu/sites/default/files/directory/Welch-Dan.jpg';
-        description = 'Some details';
-      ></professor-card>
-      <slot name="bing"></slot>
+        description = 'Some details';>
+        <p slot="bing">Please don't make me take another Java course Andrew Sears</p>
+      </professor-card>
     `;
   }
 }
